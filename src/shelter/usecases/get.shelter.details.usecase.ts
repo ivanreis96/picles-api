f01c944195b3461272c62e80b6d3b38ca88cb88a
ivanreis96 <ivreis96@gmail.com>
2024-04-04 22:33:10 -0300

    Criando interface de conexão com o MongoDB

--- src/shelter/usecases/get.shelter.details.usecase.ts
+++ src/shelter/usecases/get.shelter.details.usecase.ts
@@ -4,7 +4,7 @@ import GetShelterDetailsUseCaseOutput from "./dtos/get.shelter.details.usecase.o
 export default class GetShelterDetailsUseCase implements IUseCase<null, GetShelterDetailsUseCaseOutput>{
     run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
         return Promise.resolve(new GetShelterDetailsUseCaseOutput({
-            shelterName: "Abrigo bigo",
+            shelterName: "Gatos do cemitério",
             shelterEmail: 'abb@gmail.com',
             shelterPhone: '19982853590',
             shelterWhatsApp: '19982853590',
