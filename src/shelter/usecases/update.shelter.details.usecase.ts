da814b3c43ab4b2da46ca5493f91aa545b2883d8
ivanreis96 <ivreis96@gmail.com>
2024-04-08 21:16:35 -0300

    finalização de shelter

--- src/shelter/usecases/update.shelter.details.usecase.ts
+++ src/shelter/usecases/update.shelter.details.usecase.ts
@@ -1,11 +1,37 @@
+import { Inject, Injectable } from "@nestjs/common";
 import UpdateShelterDetailsUseCaseInput from "../dtos/update.shelter.usecase.input";
 import UpdateShelterDetailsUseCaseOutput from "../dtos/update.shelter.usecase.output";
 import { IUseCase } from "src/domain/iusecase.interface";
+import ShelterTokens from "../shelter.tokens";
+import { ShelterRepository } from "../shelter.repository";
+import { Shelter } from "../schemas/shelter.schema";
+import IShelterRepository from "../interfaces/shelter.repository.interface";
 
-export default class UpdateShelterDetailsUsecase implements IUseCase<UpdateShelterDetailsUseCaseInput, UpdateShelterDetailsUseCaseOutput>
+@Injectable()
 
-{
-    run(input: UpdateShelterDetailsUseCaseInput): Promise<UpdateShelterDetailsUseCaseOutput>{
-        throw new Error("Method not implemented.");
+export default class UpdateShelterDetailsUsecase implements IUseCase<UpdateShelterDetailsUseCaseInput, UpdateShelterDetailsUseCaseOutput>{
+
+
+    constructor(
+        @Inject(ShelterTokens.shelterRepository)
+        private readonly shelterRepository: IShelterRepository
+    ) { }
+
+
+    async run(input: UpdateShelterDetailsUseCaseInput): Promise<UpdateShelterDetailsUseCaseOutput>{
+        
+        await this.shelterRepository.update(input)
+
+        const shelter = await this.shelterRepository.get()
+
+        return new UpdateShelterDetailsUseCaseOutput({
+          
+            name: shelter.name,
+            phone: shelter.phone,
+            whatsapp: shelter.whatsApp,
+            email: shelter.email,
+            updatedAt: shelter.updatedAt,
+            createdAt: shelter.createdAt
+        })
     }
 }
